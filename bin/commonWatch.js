df3cdf671478d02f0af8aaa6413ed0275b07bbdc
luke <luke@solidstategroup.com>
2016-08-02 14:48:54 +0100

    Common watch sync task

---
+++ bin/commonWatch.js
@@ -0,0 +1,29 @@
+var gulp = require('gulp');
+var watch = require('gulp-watch');
+var clean = require('gulp-clean');
+var fs = require('fs');
+
+var commonPath = 'src/js/common';
+
+watch('../' + commonPath + '/**/*.*', { events: ['add', 'change'] }, function(file) {
+  var oldPath = file.path.substring(file.path.lastIndexOf(commonPath));
+  var newPath = 'mobile/common' + oldPath.substr(commonPath.length);
+
+  newPath = newPath.substr(0, newPath.lastIndexOf("/"));
+
+  console.log(oldPath + " copied to " + newPath);
+  gulp.src('../' + oldPath)
+    .pipe(gulp.dest('../' + newPath));
+});
+
+gulp.task('clean-files', function() {
+  return gulp.src(['../mobile/common/**/*.*'])
+    .pipe(clean());
+});
+
+gulp.task('copy-files', ['clean-files'], function() {
+  gulp.src(['../' + commonPath + '/**/*.*'])
+    .pipe(gulp.dest('../mobile/common'));
+});
+
+gulp.start('copy-files');
--- mobile/common/data/config.js
+++
@@ -1,9 +0,0 @@
-import data from './base/_data';
-module.exports = {
-    get(params) {
-        return data.dummy({data:{}});
-    },
-    getOtherStuff(params) {
-        return data.get(`Project.api.live/user/${params.id}`);
-    }
-};
