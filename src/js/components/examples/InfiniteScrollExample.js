6794be210ba31d55650f1775c8b7f52f91459aa1
luke <luke@solidstategroup.com>
2016-08-05 11:09:27 +0100

    Infinite scroll examples now support dynamic row heights

--- src/js/components/examples/InfiniteScrollExample.js
+++ src/js/components/examples/InfiniteScrollExample.js
@@ -3,26 +3,48 @@ const InfiniteScrollExample = class extends React.Component {
 
   constructor (props, context) {
     super(props, context);
-    this.state = { data: _.range(0, 100) };
+    this.state = {
+      data: _.range(0, 100).map((num) => ({ key: num, height: this.getRandomHeight()})),
+      randomRowHeights: false,
+    };
   }
+
+  getRandomHeight() { return Math.round(Math.random() * 60 + 40); }
 
   loadMore = () => {
     this.setState({
-      isLoading: true
+      isLoading: true,
     });
     setTimeout(()=> {
       const length = this.state.data.length;
       this.setState({
         isLoading: false,
-        data: this.state.data.concat(_.range(length, length + 100))
+        data: this.state.data.concat(_.range(length, length + 100)
+              .map((num) => ({ key: num, height: this.getRandomHeight()})))
       });
     }, 2000);
   }
 
+  rowHeight = ({ index }) => {
+    if (!this.state.randomRowHeights) {
+      return 40;
+    }
+
+    return index < this.state.data.length ? this.state.data[index].height : 40;
+  }
+
+  onRandomRowHeightsChanged = (randomRowHeights) => {
+    // @TODO Two updates happening here.. recomputeRowHeights() calls forceUpdate()
+    this.setState({ randomRowHeights });
+    this.virtualScroll.recomputeRowHeights();
+  }
+
+  virtualScrollRef = (virtualScroll) => this.virtualScroll = virtualScroll;
+
   renderRow (data) {
     return (
-      <div className=".FlexTable__row" key={data}>
-        Row {data + 1}
+      <div className=".FlexTable__row" key={data.key}>
+        Row {data.key + 1}
       </div>
     );
   }
@@ -42,14 +64,17 @@ const InfiniteScrollExample = class extends React.Component {
               Loading...
             </div>
           )}
-          rowHeight={40}
+          rowHeight={this.rowHeight}
           containerHeight={200}
           threshold={50}
           data={this.state.data}
           isLoading={this.state.isLoading}
           loadMore={this.loadMore}
           renderRow={this.renderRow}
+          virtualScrollRef={this.virtualScrollRef}
         />
+        Use random row heights ?
+        <Switch value={this.state.randomRowHeights} onChange={this.onRandomRowHeightsChanged}/>
       </div>
     );
   }
