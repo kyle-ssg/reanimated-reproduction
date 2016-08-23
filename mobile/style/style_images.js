6a497c7310c5cb2561a16b23f245a25ab4d471f9
Niall Quinn <niall@solidstategroup.com>
2016-08-23 12:08:28 +0100

    added image base styles for mobile

---
+++ mobile/style/style_images.js
@@ -0,0 +1,22 @@
+/**
+ * Created by niallquinn on 23/08/2016.
+ */
+
+module.exports = StyleSheet.create({
+
+  imgFluid: {
+      width: styleVariables.baseImageWidth,
+      height: styleVariables.baseImageHeight,
+      flex: 1,
+      resizeMode: 'contain',
+  },
+
+  imgRounded: {
+    borderRadius: styleVariables.baseImageHeight / 2,
+    height: styleVariables.baseImageHeight,
+    width: styleVariables.baseImageWidth,
+    borderColor: pallette.secondary,
+    borderWidth: 2,
+  }
+
+});
