c122d9a3cb265bfc50d160b3710a97f378d3bcb1
andrewmoffatt <andrew@solidstategroup.com>
2016-09-06 13:52:43 +0100

    indent 4 revision

--- mobile/components/examples/LoginExample/LoggedIn.js
+++ mobile/components/examples/LoginExample/LoggedIn.js
@@ -1,50 +1,50 @@
 import FireAuth from '../../../common/fire-auth';
 
 const LoggedIn = class extends React.Component {
-  constructor (props, context) {
-    super(props, context);
-    this.props.user.getToken()
-      .then((token)=> {
-        fetch('https://f5269c1e.ngrok.io/fb/test', {
-          method: 'GET',
-          body: null,
-          headers: {
-            'Authorization': `Bearer ${token}`
-          }
-        });
-      });
-  }
-
-  render () {
-    const { profile } = this.props;
-    const logout = <Button onPress={FireAuth.logout}><Text>Logout</Text></Button>;
-
-    if (!profile) {
-      return <Loader/>;
+    constructor(props, context) {
+        super(props, context);
+        this.props.user.getToken()
+            .then((token)=> {
+                fetch('https://f5269c1e.ngrok.io/fb/test', {
+                    method: 'GET',
+                    body: null,
+                    headers: {
+                        'Authorization': `Bearer ${token}`
+                    }
+                });
+            });
     }
 
-    return profile.emailVerified ? (
-      <Flex>
-        <Text>
-          Hi {profile.email}
-        </Text>
-        {logout}
-      </Flex>
-    ) : <Flex>
-      <Text>
-        Your email address is not verified!
-      </Text>
-      <Button onPress={FireAuth.resendVerification}>
-        <Text>Resend Verification Email</Text>
-      </Button>
-      {logout}
-    </Flex>;
-  }
+    render() {
+        const {profile} = this.props;
+        const logout = <Button onPress={FireAuth.logout}><Text>Logout</Text></Button>;
+
+        if (!profile) {
+            return <Loader/>;
+        }
+
+        return profile.emailVerified ? (
+            <Flex>
+                <Text>
+                    Hi {profile.email}
+                </Text>
+                {logout}
+            </Flex>
+        ) : <Flex>
+            <Text>
+                Your email address is not verified!
+            </Text>
+            <Button onPress={FireAuth.resendVerification}>
+                <Text>Resend Verification Email</Text>
+            </Button>
+            {logout}
+        </Flex>;
+    }
 };
 
 LoggedIn.propTypes = {
-  user: OptionalObject,
-  profile: OptionalObject,
+    user: OptionalObject,
+    profile: OptionalObject,
 };
 
 export default LoggedIn;
