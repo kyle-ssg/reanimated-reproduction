05bda9ef819490c06fb7ff212c97dc39cdc7d2f7
andrewmoffatt <andrew@solidstategroup.com>
2016-09-08 11:31:01 +0100

    refactor buttons

--- mobile/style/project/style_buttons.js
+++ mobile/style/project/style_buttons.js
@@ -1,19 +1,4 @@
 module.exports = {
-    loginButtonContainer: {
-      padding: base.paddingBaseSmall
-    },
-    loginButton: {
-      borderRadius: 20,
-      backgroundColor: 'transparent',
-      height: em(3),
-      alignSelf: 'stretch',
-      flex: 1
-    },
-    loginButtonText: {
-      color: 'white',
-      fontWeight: '500'
-    },
-
     button:{
       height:base.buttonHeight
     },
