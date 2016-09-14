4feba75c5875ab4ff6f456f062a65643125ac38e
kylejohnson <kyle@solidstategroup.com>
2016-09-14 22:55:17 +0100

    Public path to allow deep routing

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -11,6 +11,7 @@ module.exports = {
         './src/js/main.js',
     ],
     output: {
+        publicPath:'/',
         path: path.join(__dirname, '../build'),
         filename: '[name].js'
     },
