346681f87dda0628d0d1cd5bd251ec584bdc61e7
andrewmoffatt <andrew@solidstategroup.com>
2018-09-21 16:43:38 +0100

    clearing house + hero BEM + responsive breakpoints for components (sass mapping)

--- web/components/pages/HomePage.js
+++ web/components/pages/HomePage.js
@@ -15,9 +15,22 @@ export default hot(module)(class extends React.Component {
                 <AccountProvider onLogout={this.onLogout} onLogin={this.onLogin}>
                     {() => (
                         <div>
-                            <h1>
-                                Home
-                            </h1>
+                            <div className="hero">
+                                <h1 className="hero__title">Hero text</h1>
+                                <img src="https://placehold.it/900x400" className="hero__image img-fluid"/>
+                                <div className="hero__text">
+                                    <p className="hero__text__intro">Hero text intro</p>
+                                    <p className="hero__text__body">Hero text body</p>
+                                </div>
+                            </div>
+                            <div className="hero hero--blue">
+                                <h1 className="hero__title">Hero text</h1>
+                                <img src="https://placehold.it/900x400" className="hero__image img-fluid"/>
+                                <div className="hero__text">
+                                    <p className="hero__text__intro">Hero text intro</p>
+                                    <p className="hero__text__body">Hero text body</p>
+                                </div>
+                            </div>
                         </div>
                     )}
                 </AccountProvider>
