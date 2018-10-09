7f497451cc01b3a7411d876e1d25b80acbeb81ab
kyle <kyle@solidstategroup.com>
2018-10-09 14:00:51 +0100

    Fix input group

--- web/components/base/forms/InputGroup.js
+++ web/components/base/forms/InputGroup.js
@@ -1,53 +1,65 @@
-import React from 'react';
+import React, { Component } from 'react';
 import propTypes from 'prop-types';
 
-const InputGroup = window.InputGroup = ({
-                    className,
-                    disabled,
-                    id = Utils.GUID(),
-                    inputProps,
-                    isValid,
-                    onChange,
-                    placeholder,
-                    title,
-                    value,
-                }) => (
-    <div className={"form-group"}>
-        <label htmlFor={id} className="cols-sm-2 control-label">{title}</label>
 
+class InputGroup extends Component {
+
+  render() {
+    const {
+      props: {
+        disabled,
+        id = Utils.GUID(),
+        inputProps,
+        isValid,
+        onChange,
+        placeholder,
+        title,
+        value,
+      },
+    } = this;
+    return (
+      <div className="form-group">
+        <label htmlFor={id} className="cols-sm-2 control-label">{title}</label>
         {inputProps && inputProps.error && (
-            <span>
-				<span> - </span>
-				<span id={inputProps.name ? inputProps.name + "-error" : ''} className={"text-danger"}>
-					{inputProps.error}
-				</span>
-			</span>
+          <span>
+            <span> - </span>
+            <span id={inputProps.name ? inputProps.name + "-error" : ''} className={"text-danger"}>
+              {inputProps.error}
+            </span>
+          </span>
         )}
 
         <div>
-            <div>
-                <Input ref="input" {...inputProps}
-                       isValid={isValid}
-                       disabled={disabled}
-                       value={value}
-                       onChange={onChange}
-                       id={id}
-                       placeholder={placeholder}/>
-            </div>
+          <div>
+            <Input ref="input" {...inputProps}
+                   isValid={isValid}
+                   disabled={disabled}
+                   value={value}
+                   onChange={onChange}
+                   id={id}
+                   placeholder={placeholder}
+            />
+          </div>
         </div>
-    </div>
-);
+      </div>
+    );
+  }
+}
+
+window.InputGroup = InputGroup;
 
 InputGroup.defaultProps = {};
 
 InputGroup.propTypes = {
-    className: propTypes.string,
-    disabled: propTypes.bool,
-    id: propTypes.string,
-    inputProps: propTypes.object,
-    isValid: propTypes.bool,
-    onChange: propTypes.func,
-    placeholder: propTypes.string,
-    title: propTypes.string,
-    value: propTypes.string,
+  className: propTypes.string,
+  disabled: propTypes.bool,
+  id: propTypes.string,
+  inputProps: propTypes.object,
+  isValid: propTypes.bool,
+  onChange: propTypes.func,
+  placeholder: propTypes.string,
+  title: propTypes.string,
+  value: propTypes.string,
 };
+
+export default InputGroup;
