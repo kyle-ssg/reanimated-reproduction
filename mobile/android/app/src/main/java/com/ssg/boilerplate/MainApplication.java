dc082bb760edfc2b5f6bd22f45645aca8894d3dd
Luke Fanning <luke@solidstategroup.com>
2019-01-10 12:01:46 +0000

    Cleaning up

--- mobile/android/app/src/main/java/com/ssg/boilerplate/MainApplication.java
+++ mobile/android/app/src/main/java/com/ssg/boilerplate/MainApplication.java
@@ -1,51 +1,39 @@
-//STANDARD REACT_NATIVE STUFF
 package com.ssg.boilerplate;
-import android.app.Application;
-import com.facebook.react.ReactApplication;
-import com.BV.LinearGradient.LinearGradientPackage;
-import com.clipsub.rnbottomsheet.RNBottomSheetPackage;
-import com.wix.interactable.Interactable;
-import com.facebook.react.ReactNativeHost;
+
+import android.support.annotation.Nullable;
+
 import com.facebook.react.ReactPackage;
-import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
+
 import java.util.Arrays;
 import java.util.List;
 
-//REAT_NATIVE_NAVIGATION
- import com.reactnativenavigation.NavigationApplication;
-import android.support.annotation.Nullable;
+import com.reactnativenavigation.NavigationApplication;
 
+// This is here for 'react-native link'
+// import com.facebook.react.ReactApplication;
 
-//REACT_NATIVE_BRANCH
 import io.branch.rnbranch.RNBranchPackage;
 import io.branch.referral.Branch;
 
-//REACT_NATIVE_CRASHLYTICS
+import com.wix.interactable.Interactable;
+
 import com.smixx.fabric.FabricPackage;
 import com.crashlytics.android.Crashlytics;
 import io.fabric.sdk.android.Fabric;
 
-//REACT_NATIVE_FIREBASE
 import io.invertase.firebase.RNFirebasePackage;
 import io.invertase.firebase.messaging.RNFirebaseMessagingPackage; // <-- Add this line
 import io.invertase.firebase.analytics.RNFirebaseAnalyticsPackage; // <-- Add this line
 import io.invertase.firebase.notifications.RNFirebaseNotificationsPackage; // <-- Add this line
 
-import com.oblador.vectoricons.VectorIconsPackage;
 import com.learnium.RNDeviceInfo.RNDeviceInfo;
-
-//REACT_NATIVE_LOTTIE
 import com.airbnb.android.react.lottie.LottiePackage;
-
-//VECTOR_ICONS
 import com.oblador.vectoricons.VectorIconsPackage;
-
-//REACT_NATIVE_DEVICE_INFO
 import com.learnium.RNDeviceInfo.RNDeviceInfo;
-
-//REACT_NATIVE_VIEW_OVERFLOW
 import com.entria.views.RNViewOverflowPackage;
+import com.BV.LinearGradient.LinearGradientPackage;
+import com.clipsub.rnbottomsheet.RNBottomSheetPackage;
 
 public class MainApplication extends NavigationApplication {
     @Override
@@ -56,35 +44,37 @@ public class MainApplication extends NavigationApplication {
     @Nullable
     @Override
     public List<ReactPackage> createAdditionalReactPackages() {
-           return Arrays.<ReactPackage>asList(
-                                 new RNBottomSheetPackage(),
-                                 new Interactable(),
+        return Arrays.<ReactPackage>asList(
+            // the following line is present so that `react-native link` works correctly
+            // new MainReactPackage(),
+            new RNViewOverflowPackage(),
+            new RNBottomSheetPackage(),
+            new Interactable(),
 
-                               //REACT_NATIVE_FIRE_BASE
-                                   new RNFirebasePackage(),
-                                   new RNFirebaseMessagingPackage(),
-                                   new RNFirebaseAnalyticsPackage(),
-                                   new RNFirebaseNotificationsPackage(),
-                               //END OF REACT_NATIVE_FIRE_BASE
+            //REACT_NATIVE_FIRE_BASE
+            new RNFirebasePackage(),
+            new RNFirebaseMessagingPackage(),
+            new RNFirebaseAnalyticsPackage(),
+            new RNFirebaseNotificationsPackage(),
+            //END OF REACT_NATIVE_FIRE_BASE
 
-                               new FabricPackage(),
-                               new LottiePackage(),
-                               new VectorIconsPackage(),
-                               new RNDeviceInfo(),
-                               new RNBranchPackage(),
-                               new LinearGradientPackage(),
-                               new RNViewOverflowPackage()
-                        );
+            new FabricPackage(),
+            new LottiePackage(),
+            new VectorIconsPackage(),
+            new RNDeviceInfo(),
+            new RNBranchPackage(),
+            new LinearGradientPackage()
+        );
     }
 
     @Override public
     String getJSMainModuleName() { return "index"; }
 
 
- @Override
-  public void onCreate() {
+    @Override
+    public void onCreate() {
         super.onCreate();
         Branch.getAutoInstance(this);
-  }
+    }
 
 }
