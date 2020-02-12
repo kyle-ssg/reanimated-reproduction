d6724ef49dfb7fe2da64ad1da0b0c994414bcd24
kyle <kyle@solidstategroup.com>
2020-02-12 17:59:00 +0000

    Realtime monitoring

---
+++ common/providers/withPerformance.js
@@ -0,0 +1,83 @@
+import React, { Component } from 'react';
+
+const Profiler = React.Profiler;
+
+export default (WrappedComponent, _id, remote) => {
+    class HOC extends Component {
+        static displayName = 'withPerformance';
+
+        constructor(props) {
+            super(props);
+            this.trace = null;
+            this.traceStarted = false;
+            this.initialMount = null;
+            this.initialUpdates = null;
+        }
+
+        async componentDidMount() {
+            // this.trace = firebase.perf().newTrace('cache_trace');
+            // await this.trace.start();
+            // this.traceStarted = true;
+            // // put cached values after trace started
+            // await this.trace.putAttribute('mount_time', this.initialMount);
+            // if (this.initialUpdates) await this.trace.incrementMetric('updates', this.initialUpdates);
+        }
+
+        async componentWillUnmount() {
+            if (this.trace) {
+                await this.trace.stop();
+            }
+        }
+
+        // id, // the "id" prop of the Profiler tree that has just committed
+        // phase, // either "mount" (if the tree just mounted) or "update" (if it re-rendered)
+        // actualDuration, // time spent rendering the committed update
+        // baseDuration, // estimated time to render the entire subtree without memoization
+        // startTime, // when React began rendering this update
+        // commitTime, // when React committed this update
+        // interactions // the Set of interactions belonging to this update
+
+        logMeasurement = async (id, phase, actualDuration, baseDuration) => {
+            // see output during DEV
+            if (__DEV__) console.log(id, actualDuration);
+            if (remote) {
+                fetch(remote, {
+                    method: 'POST',
+                    headers:{
+                        "Content-Type":"application/json"
+                    },
+                    body: JSON.stringify({ value: actualDuration }),
+                });
+            }
+            // if (phase === 'mount') {
+            //     if (this.traceStarted) {
+            //         await this.trace.putAttribute('mount_time', actualDuration);
+            //     } else {
+            //         // cache mount time before trace.start()
+            //         this.initialMount = actualDuration;
+            //     }
+            // } else if (phase === 'update') {
+            //     if (this.traceStarted) {
+            //         await this.trace.incrementMetric('updates', 1);
+            //     } else {
+            //         // cache updates before trace.start()
+            //         this.initialUpdates = this.initialUpdates + 1;
+            //     }
+            // }
+        }
+
+        render() {
+            return (
+                <Profiler id={_id} onRender={this.logMeasurement}>
+                    <WrappedComponent
+                      {...this.props}
+                      {...this.state}
+                    />
+                </Profiler>
+
+            );
+        }
+    }
+
+    return HOC;
+};
--- mobile/app/screens/HomeScreen.js
+++ mobile/app/screens/HomeScreen.js
@@ -1,8 +1,44 @@
 /**
  * Created by kylejohnson on 28/01/2017.
  */
-import React, { Component } from 'react';
+import React, { Component, PureComponent } from 'react';
 import propTypes from 'prop-types';
+import withPerformance from '../../../common/providers/withPerformance';
+
+// Automatically sets relevant head tags for seo, see _app for the remainder tags
+const Item = class extends Component {
+    static displayName = 'Item';
+
+
+    constructor(props) {
+        super(props)
+        console.log(this.props.index);
+    }
+
+    componentDidUpdate() {
+        console.log('test');
+    }
+
+    render() {
+        // const Comp = ReactNative.Text;
+        const Comp = Text;
+        return (
+            <View style={{
+                margin: 20,
+            }}
+            >
+                <View>
+                    {/* <ION style={{ color: "red" }} name="ios-chatboxes"/> */}
+                    <Row>
+                        <Comp>test</Comp>
+                        <Comp>test dsad sads ad ad asd asd asdsa dasd sad asd asd asd sad sadsa dasdasd asd sada</Comp>
+                        <Comp>test dsad sads ad ad asd asd asdsa dasd sad asd asd asd sad sadsa dasdasd asd sada</Comp>
+                    </Row>
+                </View>
+            </View>
+        );
+    }
+};
 
 const TermsScreen = class extends Component {
   static displayName = 'TermsScreen';
@@ -14,33 +50,79 @@ const TermsScreen = class extends Component {
   constructor(props, context) {
       super(props, context);
       this.state = {
+          data: this.getData(),
       };
   }
 
+  getData = () => _.range(0, 1000).map((i) => ({
+      id: i,
+      'glossary': {
+          'title': 'example glossary',
+          'GlossDiv': {
+              'title': 'S',
+              'GlossList': {
+                  'GlossEntry': {
+                      'ID': 'SGML',
+                      'SortAs': 'SGML',
+                      'GlossTerm': 'Standard Generalized Markup Language',
+                      'Acronym': 'SGML',
+                      'Abbrev': 'ISO 8879:1986',
+                      'GlossDef': {
+                          'para': 'A meta-markup language, used to create markup languages such as DocBook.',
+                          'GlossSeeAlso': ['GML', 'XML'],
+                      },
+                      'GlossSee': 'markup',
+                  },
+              },
+          },
+      },
+  }))
+
   componentDidMount() {
       Navigation.events().bindComponent(this);
+      setTimeout(() => {
+          this.setState({
+              ready: true,
+          });
+      }, 200);
   }
 
-  navigateArticle = (id) => {
-      Navigation.push(this.props.componentId, routes.articleScreen(id, 'Article'));
-  }
+    refreshData =() => {
+        this.setState({ data: this.getData() });
+        // setTimeout(() => {
+        //     this.setState({ data: this.getData() });
+        // }, 5);
+        // setTimeout(() => {
+        //     this.setState({ data: this.getData() });
+        // }, 10);
+    }
 
-  // eslint-disable-next-line class-methods-use-this
-  componentDidAppear() {
-      API.trackPage('Home');
-  }
+    render() {
+        return (
+            <Flex style={[Styles.body]}>
+                <Button onPress={this.refreshData}>Refresh</Button>
+                {this.state.ready && (
+                    <FlatList
+                      initialNumToRender={4}
+                      data={this.state.data}
+                      renderItem={({ item, index }) => (
+                          <View>
+                              <Item
+                                index={index}
+                                item={item}
+                              />
+                          </View>
+                      )}
+                    />
+                )}
 
-  render() {
-      return (
-          <Flex style={[Styles.body]}>
-              <Text>hi</Text>
-          </Flex>
-      );
-  }
+            </Flex>
+        );
+    }
 };
 
 // const styles = StyleSheet.create({
 //
 // });
 
-module.exports = TermsScreen;
+module.exports = withPerformance(TermsScreen, 'Terms Screen', 'http://localhost:3000/value');
