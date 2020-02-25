23d4342567740d85a2e3c616f83e0b7eca6a0a30
kyle <kyle@solidstategroup.com>
2020-02-25 15:24:44 +0000

    tidy up reducer and saga

--- common/reducer.js
+++ common/reducer.js
@@ -1,5 +1,7 @@
 import './utils';
+import filter from 'lodash/filter';
 
+// Sets item in reducer as loading, clears error for that item
 const itemLoading = (state, prefix, action) => {
     return {
         ...state,
@@ -8,23 +10,8 @@ const itemLoading = (state, prefix, action) => {
     };
 };
 
-const itemSaved = (state, prefix, action) => {
-    if (action.index) { // Item is part of a collection, add it within the prefix
-        return {
-            ...state,
-            [prefix]: { ...state[prefix], [action.index]: action.data },
-            [`${prefix}Loading`]: false,
-            [`${prefix}Error`]: null,
-        };
-    }
-    return {
-        ...state,
-        [`${prefix}Error`]: null,
-        [prefix]: action.data,
-        [`${prefix}Loading`]: false,
-    };
-};
-
+// todo: perhaps we need loading/errors to be based on particular ids i.e. dont show user 2 as saving if it's just user 1
+// Sets item in reducer as saved, clears error for that item
 const itemLoaded = (state, prefix, action) => {
     if (action.index) { // Item is part of a collection, add it within the prefix
         return {
@@ -39,6 +26,22 @@ const itemLoaded = (state, prefix, action) => {
         [`${prefix}Error`]: null,
         [`${prefix}Loading`]: false,
         [prefix]: action.data };
+};
+
+// Adds an item to the reducer collection, if one exists with the same ID it will be updated
+const appendItem = (state, prefix, action) => {
+    return {
+        ...state,
+        [prefix]: filter(state[prefix], i => i.id !== action.id || action.data.id).concat([action.data]),
+    };
+};
+
+// Removes an item from a collection based on an ID
+const deleteItem = (state, prefix, action) => {
+    return {
+        ...state,
+        [prefix]: filter(state[prefix], i => i.id !== action.id),
+    };
 };
 
 const itemError = (state, prefix, action) => {
@@ -61,39 +64,30 @@ function defaultReducer(
         // API.log('DISPATCHER', action.type);
     }
     switch (action.type) {
-        case Actions.LOGIN:
-            return itemLoading(state, 'user', action);
         case Actions.LOGIN_LOADED:
-        case Actions.REFRESH_USER_LOADED:
-            return itemLoaded(state, 'user', action);
-        case Actions.LOGIN_ERROR:
-            return itemError(state, 'user', action);
-        case Actions.REGISTER:
-            return itemLoading(state, 'user', action);
+        case Actions.UPDATE_USER_LOADED:
+        case Actions.CONFIRM_EMAIL_LOADED:
         case Actions.REGISTER_LOADED:
             return itemLoaded(state, 'user', action);
-        case Actions.REGISTER_ERROR:
-            return itemError(state, 'user', action);
-        case Actions.CONFIRM_EMAIL:
-            return itemLoading(state, 'user', action);
-        case Actions.CONFIRM_EMAIL_LOADED:
-            return itemSaved(state, 'user', action);
+        case Actions.LOGIN_ERROR:
         case Actions.CONFIRM_EMAIL_ERROR:
-            return itemError(state, 'user', action);
-        case Actions.UPDATE_USER_LOADED:
-            return itemSaved(state, 'user', action);
+        case Actions.REGISTER_ERROR:
         case Actions.UPDATE_USER_ERROR:
             return itemError(state, 'user', action);
+        case Actions.REGISTER:
+        case Actions.UPDATE_USER:
+        case Actions.CONFIRM_EMAIL:
+        case Actions.LOGIN:
+            return itemLoading(state, 'user', action);
         case Actions.CLEAR_USER:
             return { ...state, user: null };
         case Actions.STARTUP_LOADED:
             return { ...state, ...action.data };
         // END OF REDUCER
+        // KEEP THE ABOVE LINE IN, IT IS USED BY OUR CLI
         default:
             return state;
     }
 }
 
-export default (state, action) => {
-    return defaultReducer(state, action);
-};
+export default defaultReducer;
