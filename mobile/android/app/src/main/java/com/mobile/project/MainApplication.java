57107e8c02bb1c070f4ec7ab458e3e318afd9adb
kyle <kyle@solidstategroup.com>
2020-03-28 10:25:25 +0000

    Working

--- mobile/android/app/src/main/java/com/mobile/project/MainApplication.java
+++ mobile/android/app/src/main/java/com/mobile/project/MainApplication.java
@@ -1,28 +1,49 @@
 package com.mobile.project;
 
-import android.app.Application;
-import android.util.Log;
-
+import com.facebook.react.PackageList;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import androidx.annotation.Nullable;
-import com.facebook.react.BuildConfig;
+import com.reactnativenavigation.NavigationApplication;
+import com.reactnativenavigation.react.NavigationPackage;
+import com.reactnativenavigation.react.NavigationReactNativeHost;
 
 // import io.invertase.firebase.messaging.RNFirebaseMessagingPackage;
 // import io.invertase.firebase.notifications.RNFirebaseNotificationsPackage; // <-- Add this line
 // import io.invertase.firebase.analytics.RNFirebaseAnalyticsPackage; // <-- Add this line
 import com.microsoft.codepush.react.CodePush;
-import com.facebook.react.PackageList;
 
-
-import com.reactnativenavigation.NavigationApplication;
-import com.reactnativenavigation.react.NavigationReactNativeHost;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainApplication extends NavigationApplication {
+
+    private final ReactNativeHost mReactNativeHost =
+            new NavigationReactNativeHost(this) {
+                @Override
+                protected String getJSMainModuleName() {
+                    return "index";
+                }
+
+                @Override
+                protected String getJSBundleFile() {
+                    return CodePush.getJSBundleFile();
+                }
+
+                @Override
+                public boolean getUseDeveloperSupport() {
+                    return BuildConfig.DEBUG;
+                }
+
+                @Override
+                public List<ReactPackage> getPackages() {
+                    ArrayList<ReactPackage> packages = new PackageList(this).getPackages();
+
+                    //              packages.add(new RNFirebaseNotificationsPackage());
+                    return packages;
+                }
+            };
+
 
     @Override
     public void onCreate() {
@@ -30,35 +51,7 @@ public class MainApplication extends NavigationApplication {
     }
 
     @Override
-    protected ReactNativeHost createReactNativeHost() {
-        return new NavigationReactNativeHost(this) {
-            @Override
-            protected String getJSMainModuleName() {
-                return "index";
-            }
-
-            @Override
-            protected String getJSBundleFile() {
-                return CodePush.getJSBundleFile();
-            }
-        };
-    }
-
-    @Override
-    public boolean isDebug() {
-        return BuildConfig.DEBUG;
-    }
-
-    @Nullable
-    @Override
-    public List<ReactPackage> createAdditionalReactPackages() {
-         @SuppressWarnings("UnnecessaryLocalVariable")
-             List<ReactPackage> packages = new PackageList(this).getPackages();
-             // Packages that cannot be autolinked yet can be added manually here, for example:
-//              packages.add(new RNFirebaseMessagingPackage());
-//              packages.add(new RNFirebaseNotificationsPackage());
-//              packages.add(new RNFirebaseAnalyticsPackage());
-             // packages.add(new MyReactNativePackage());
-             return packages;
+    public ReactNativeHost getReactNativeHost() {
+        return mReactNativeHost;
     }
 }
