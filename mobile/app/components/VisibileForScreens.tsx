b626f10dbbaab7fab5f1714babcbdc9cb1030cb3
kyle <kyle@solidstategroup.com>
2020-07-23 20:44:58 +0100

    Items

--- mobile/app/components/VisibileForScreens.tsx
+++ mobile/app/components/VisibileForScreens.tsx
@@ -1,11 +1,8 @@
-import React, { FunctionComponent, ReactNode, useCallback, useEffect, useState } from 'react'; // we need this to make JSX compile
+import React, { FunctionComponent, ReactNode, useEffect, useState } from 'react'; // we need this to make JSX compile
 import { connect } from 'react-redux';
 // eslint-disable-next-line no-unused-vars
-import { AppState } from '../state-type';
+import { AppState } from 'common/state-type';
 import { RouteUrls } from '../route-urls';
-import { boolean } from '@storybook/addon-knobs';
-import { StyleProp } from 'react-native';
-import { SafeAreaInsetsContext, SafeAreaProvider, initialWindowMetrics } from 'react-native-safe-area-context';
 
 type ComponentType = {
     activeScreen: AppState["activeScreen"];
@@ -15,17 +12,18 @@ type ComponentType = {
     children: ReactNode;
 }
 
+//Use this to make an absolute view persist across multiple native screens
 const VisibleForScreens: FunctionComponent<ComponentType> = ({ children,activeScreen, style, target, navigator="root" }) => {
-    const [isActive, setIsActive] = useState<boolean>()
+    const [isActive, setIsActive] = useState<boolean>(false)
     useEffect(()=> {
         if (activeScreen) {
             setIsActive(target.includes(activeScreen[navigator]))
         }
     },[activeScreen, activeScreen])
     return (
-      <Fade style={style} value={isActive}>
-          {children}
-      </Fade>
+        <Fade style={style} value={isActive}>
+            {children}
+        </Fade>
     );
 };
 
--- mobile/src/global.d.ts
+++ mobile/src/global.d.ts
@@ -1,11 +1,9 @@
 /// <reference types="react-native" />
-import { NamedStyles } from 'react-native';
-
 declare let View: React.ComponentType<ReactNative.ViewProps>;
 declare let Flex: React.ComponentType<ReactNative.ViewProps>;
 declare let TouchableOpacity: React.ComponentType<ReactNative.TouchableOpacityProps>;
 declare let Text: React.ComponentType<ReactNative.TextProps>;
-declare let Fade: React.ComponentType<ReactNative.ViewProps> & {value:boolean};
+declare let Fade: React.ComponentType<ReactNative.ViewProps& {value:boolean; children: ReactNode}>;
 declare let StyleSheet: { create: (styles: T | NamedStyles<T>)=> T };
 declare let ScrollView: React.ComponentType<ReactNative.ScrollViewProps>;
 declare let SafeAreaView: React.ComponentType<ReactNative.ViewProps>;
