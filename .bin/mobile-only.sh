74c8f064c0f52460935f0597cce0400247df0387
kyle <kyle@solidstategroup.com>
2020-07-26 18:27:18 +0100

    mobile-only.sh

---
+++ .bin/mobile-only.sh
@@ -0,0 +1,21 @@
+rm -rf .docs
+rm -rf .next
+rm -rf .storybook
+rm -rf .webpack
+rm -rf components
+rm -rf node_modules
+rm -rf pages
+rm -rf patches
+rm -rf project
+rm -rf public
+rm -rf stories
+rm -rf styles
+rm -rf tests
+rm -rf types
+rm .nowignore
+rm babelconfig.js
+rm next.config.js
+rm next-env.d.ts
+rm now.json
+rm package.json
+rm README.md
