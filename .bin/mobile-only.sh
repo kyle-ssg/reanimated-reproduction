e203c08b39731b6d2a5c078e5cde92a98900b15b
kyle <kyle@solidstategroup.com>
2020-07-26 18:36:24 +0100

    mobile-only.sh stable

--- .bin/mobile-only.sh
+++ .bin/mobile-only.sh
@@ -13,12 +13,15 @@ rm -rf styles
 rm -rf tests
 rm -rf types
 rm .nowignore
-rm babelconfig.js
-rm next.config.js
-rm next-env.d.ts
-rm now.json
-rm package.json
-rm README.md
-rm selenium-server.log
-rm server.js
-rm tsconfig.json
+mv .eslintrc.js ./mobile/eslintrc.js
+rm ./babel.config.js
+rm ./jest.config.js
+rm ./next.config.js
+rm ./next-env.d.ts
+rm ./now.json
+rm ./package.json
+rm ./package-lock.json
+rm ./README.md
+rm ./selenium-server.log
+rm ./server.js
+rm ./tsconfig.json
