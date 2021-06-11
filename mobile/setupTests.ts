10adaae4ecfb2be9a8143072878905a8174d2b21
Jai O'Hanlon <jai@reachora.com>
2021-06-11 09:42:37 +0100

    added react test library, setup jest with /some/ global support

---
+++ mobile/setupTests.ts
@@ -0,0 +1,39 @@
+// import "./app/project/api/api";
+// import "./strings";
+//Parent globals
+
+import ReactNative from 'react-native';
+
+//React native components
+global.ActivityIndicator = ReactNative.ActivityIndicator;
+global.Button = ReactNative.Button;
+global.FlatList = ReactNative.FlatList;
+global.Image = ReactNative.Image;
+global.ImageBackground = ReactNative.ImageBackground;
+global.KeyboardAvoidingView  = ReactNative.KeyboardAvoidingView;
+global.Modal = ReactNative.Modal;
+global.SafeAreaView = ReactNative.SafeAreaView;
+global.ScrollView = ReactNative.ScrollView;
+global.SectionList = ReactNative.SectionList;
+global.StyleSheet = ReactNative.StyleSheet;
+global.Switch = ReactNative.Switch;
+global.Text = ReactNative.Text;
+global.TextInput = ReactNative.TextInput;
+global.TouchableOpacity = ReactNative.TouchableOpacity;
+global.View = ReactNative.View;
+
+// APIS
+global.Alert = ReactNative.Alert;
+global.DeviceEventEmitter = ReactNative.DeviceEventEmitter;
+global.Linking = ReactNative.Linking;
+global.PixelRatio = ReactNative.PixelRatio;
+global.Platform = ReactNative.Platform;
+
+//Device info
+global.DeviceHeight = ReactNative.Dimensions.get("window").height;
+global.DeviceWidth = ReactNative.Dimensions.get("window").width;
+
+
+// import "common/utils";
+// import "components/base";
+import "app/style/_style_screen";
