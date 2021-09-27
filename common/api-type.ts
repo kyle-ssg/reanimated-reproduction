296279941dd197923bb9843b8ebf3e6933c048b3
kyle-ssg <kyle@solidstategroup.com>
2021-09-27 17:38:22 +0100

    update api-types

--- common/api-type.ts
+++ common/api-type.ts
@@ -1,14 +1,14 @@
-import { showBottomSheetWithOptionsDTO } from 'react-native-bottomsheet'
 import { Middleware } from 'redux'
-import { FirebaseMessagingTypes } from '@react-native-firebase/messaging'
 import { IncomingMessage } from 'http'
-
 type CallbackType = (err: any, data: any) => void
 
 export interface Storage {
   init: () => Promise<any>
   clear: () => Promise<void>
-  getItem: (key: string, req: IncomingMessage | CallbackType) => Promise<string>
+  getItem: (
+    key: string,
+    req?: IncomingMessage | CallbackType,
+  ) => Promise<string>
   setItem: (
     key: string,
     value: string,
@@ -32,25 +32,8 @@ export type APIType = {
   setStoredToken: (val: string) => Promise<void>
   setStoredRefreshToken: (val: string) => Promise<void>
   storage: Storage
+  noPicksAvailable: any
+  getPixelRatio: () => number
   auth: { Cognito: any } // todo
   trackEvent: (data) => void // TODO
-  showOptions?: {
-    title: string
-    _options: string[]
-    cancelButton?: boolean
-    dark?: boolean
-    destructiveOption?: boolean
-    resolveCancel?: (boolean) => Promise<number>
-  }
-  push?: {
-    //todo - cant use Remote message directly
-    getInitialNotification: () => Promise<FirebaseMessagingTypes.RemoteMessage | null>
-    subscribe: (topic: string) => Promise<void>
-    unsubscribe: (topic: string) => Promise<void>
-    stop: () => void
-    init: (
-      onNotification?: (res: FirebaseMessagingTypes.RemoteMessage) => void,
-      silent?: boolean,
-    ) => Promise<string>
-  }
 }
