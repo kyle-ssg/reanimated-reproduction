ca378cea07ec0f7fff0b50361d2dc78fa9ef61ce
kyle-ssg <kyle@solidstategroup.com>
2021-11-01 21:38:04 +0000

    remove index.e2e

--- .webpack/plugins.js
+++ .webpack/plugins.js
@@ -1,10 +1,5 @@
 const webpack = require('webpack');
 
-
-
 module.exports = [
     new webpack.DefinePlugin(require('../.env/environment-variables')),
-    // Fixes warning in moment-with-locales.min.js
-    // Module not found: Error: Can't resolve './locale' in ...
-
 ];
