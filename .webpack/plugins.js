1a511990947b489e325516f460e5e44485e101fa
kyle-ssg <kyle@solidstategroup.com>
2021-11-01 20:44:48 +0000

    updates

--- .webpack/plugins.js
+++ .webpack/plugins.js
@@ -6,6 +6,5 @@ module.exports = [
     new webpack.DefinePlugin(require('../.env/environment-variables')),
     // Fixes warning in moment-with-locales.min.js
     // Module not found: Error: Can't resolve './locale' in ...
-    new webpack.IgnorePlugin(/^\.\/locale$/, /moment\/min$/),
 
 ];
