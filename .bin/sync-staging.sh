844b4cb2c897eacc165f21eff0120b2c9adaa547
kyle-ssg <kyle@solidstategroup.com>
2022-02-13 16:06:39 +0000

    test

--- .bin/sync-staging.sh
+++ .bin/sync-staging.sh
@@ -3,5 +3,6 @@ SOURCE=master
 TARGET=mobile/staging/$VERSION
 
 #This ensures that a staging/$VERSION exists whenever master is pushed to, so that native versions can be patched
-git checkout -b $TARGET | echo "Branch already exists." && exit;
+git checkout -b $TARGET | git checkout $TARGET;
+echo "Checked out $TARGET"
 #git push HEAD:$TARGET --no-verify --force -o ci.skip
