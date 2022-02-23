7b4413c940b67ac2e6dad475c2f882b820a5e44c
kyle-ssg <kyle@solidstategroup.com>
2022-02-23 21:11:20 +0000

    npm i fixes

--- mobile/.bin/postinstall.sh
+++ mobile/.bin/postinstall.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-if [ -z "$APPCENTER_BUILD_ID" && -z "$CI" ]
+if [ -z "$APPCENTER_BUILD_ID" ]&&[ -z "$CI" ]
 then
     echo "APPCENTER_BUILD_ID and CI not defined"
+    cd ../ && npm i && cd ./mobile
     cd ./ios && arch -x86_64 pod install --repo-update && cd ..
 else
     echo "APPCENTER_BUILD_ID is defined"
