b3aa86028a62e1f5996f6ee1fffb9a10a1db6dd3
kyle-ssg <kyle@solidstategroup.com>
2022-02-27 13:31:34 +0000

    Tidy

--- common/store.ts
+++ common/store.ts
@@ -2,7 +2,7 @@ import { configureStore } from '@reduxjs/toolkit'
 import { userService, userSlice } from './hooks/useUser'
 import { startupSlice } from './hooks/useStartup'
 import { getApi } from './api'
-//END OF STORE_IMPORTS
+// END OF IMPORTS
 
 let _store: any = null
 export const store = () => {
@@ -12,11 +12,12 @@ export const store = () => {
       [userService.reducerPath]: userService.reducer,
       user: userSlice.reducer,
       startup: startupSlice.reducer,
+      // END OF REDUCERS
     },
     middleware: (getDefaultMiddleware) =>
       getDefaultMiddleware()
         .concat(userService.middleware)
-        //END OF STORE_MIDDLEWARE
+        // END OF MIDDLEWARE
         .concat(getApi().middlewares || []),
   })
   return _store
