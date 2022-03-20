d69cbe3a011de9d536a86c1ca057b0f158b9de1c
kyle-ssg <kyle@solidstategroup.com>
2022-03-20 14:14:04 +0000

    Fixes

--- common/components/LanguageHandler.tsx
+++ common/components/LanguageHandler.tsx
@@ -1,30 +1,16 @@
-import { useUser } from '../hooks/useUser'
-import { FunctionComponent, useEffect } from 'react'
+import { FunctionComponent } from 'react'
 import { Constants } from '../utils'
 import { getStrings } from '../strings'
 import Strings from '../../project/localisation'
-import { API } from '../../project/api'
 
-Strings.setLanguage(API.getStoredLocale(''))
-
-const LanguageHandler: FunctionComponent<any> = ({ children }) => {
-  const { user } = useUser()
-  const locale = user?.locale
-  const forceLanguage = Constants.simulate.FORCE_LANGUAGE
-  if (forceLanguage) {
-    API.setStoredLocale(forceLanguage)
+const LanguageHandler: FunctionComponent<any> = ({
+  children,
+  defaultLocale,
+}) => {
+  const forceLanguage = Constants.simulate.FORCE_LANGUAGE || defaultLocale
+  if (Strings.getLanguage() !== forceLanguage) {
+    getStrings().setLanguage(forceLanguage)
   }
-  useEffect(() => {
-    if (forceLanguage) {
-      getStrings().setLanguage(forceLanguage)
-      return
-    }
-    if (locale && locale !== getStrings().getInterfaceLanguage()) {
-      try {
-        getStrings().setLanguage(locale)
-      } catch (e) {}
-    }
-  }, [locale, forceLanguage])
   return children
 }
 
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,79 +1,81 @@
-import { API } from 'project/api'
 import 'project/polyfill'
-import App, { AppInitialProps, AppProps } from 'next/app'
+import { AppProps } from 'next/app'
 import '../styles/Global.scss'
 import LanguageHandler from 'common/components/LanguageHandler'
 import { NextPageWithLayout } from 'types/nextPageWithLayout'
-import { nextReduxWrapper } from 'components/util/nextReduxWrapper'
 import NProgress from 'components/util/NProgress'
-import Strings from 'project/localisation'
 import Head from 'next/head'
-import { startupActions } from 'common/hooks/useStartup'
+import { useRouter } from 'next/router'
+import { FC } from 'react'
+import { nextReduxWrapper } from 'components/util/nextReduxWrapper'
+import { startupActions } from '../common/hooks/useStartup'
+import { Constants } from '../common/utils'
 
 type AppPropsWithLayout = AppProps & {
   Component: NextPageWithLayout
 }
 
-//@ts-ignore
-class WrappedApp extends App<AppInitialProps> {
-  public static getInitialProps = nextReduxWrapper.getStaticProps(
-    (store) => async (ctx) => {
-      try {
-        const locale = API.getStoredLocale(ctx.locale)
-        if (!store.getState().startup?.locale) {
-          Strings.setLanguage(locale)
-          await store.dispatch(startupActions.startup({ locale }))
-        }
-      } catch (e) {
-        console.error(e)
-      }
-      return {
-        props: {},
-      }
-    },
+type ComponentType = AppPropsWithLayout
+
+const AppComponent: FC<ComponentType> = ({ Component, pageProps }) => {
+  const getLayout = Component.getLayout || ((page) => page)
+  const router = useRouter()
+  if (router.asPath.includes('sw.js')) {
+    return null
+  }
+  return (
+    <LanguageHandler defaultLocale={router.locale || Constants.defaultLocale}>
+      <Head>
+        <meta charSet='utf-8' />
+        <meta httpEquiv='x-ua-compatible' content='ie=edge' />
+        <meta name='description' content='TheApp' />
+        <meta name='theme-color' content='#317EFB' />
+        <meta
+          name='viewport'
+          content='width=device-width, initial-scale=1, shrink-to-fit=no'
+        />
+        <link rel='apple-touch-icon' href='/images/icons-192.png' />
+        <link rel='icon' sizes='192x192' href='/images/icons-192.png' />
+        <link rel='shortcut icon' href='/images/favicon.ico' />
+        <link
+          rel='icon'
+          type='image/png'
+          sizes='32x32'
+          href='/images/favicon-32x32.png'
+        />
+        <link
+          rel='icon'
+          type='image/png'
+          sizes='16x16'
+          href='/images/favicon-16x16.png'
+        />
+        <meta property='og:title' content='TheProject' />
+      </Head>
+      <NProgress />
+      {getLayout(<Component {...pageProps} />, pageProps)}
+      <div id='modal' />
+      <div id='confirm' />
+      <div id='alert' />
+    </LanguageHandler>
   )
-  constructor(props: any) {
-    super(props)
-  }
-  public render() {
-    const { Component, pageProps }: AppPropsWithLayout = this.props
-    const getLayout = Component.getLayout || ((page) => page)
-    return (
-      <LanguageHandler>
-        <Head>
-          <meta charSet='utf-8' />
-          <meta httpEquiv='x-ua-compatible' content='ie=edge' />
-          <meta name='description' content='TheApp' />
-          <meta name='theme-color' content='#317EFB' />
-          <meta
-            name='viewport'
-            content='width=device-width, initial-scale=1, shrink-to-fit=no'
-          />
-          <link rel='apple-touch-icon' href='/images/icons-192.png' />
-          <link rel='icon' sizes='192x192' href='/images/icons-192.png' />
-          <link rel='shortcut icon' href='/images/favicon.ico' />
-          <link
-            rel='icon'
-            type='image/png'
-            sizes='32x32'
-            href='/images/favicon-32x32.png'
-          />
-          <link
-            rel='icon'
-            type='image/png'
-            sizes='16x16'
-            href='/images/favicon-16x16.png'
-          />
-          <meta property='og:title' content='TheProject' />
-        </Head>
-        <NProgress />
-        {getLayout(<Component {...pageProps} />, pageProps)}
-        <div id='modal' />
-        <div id='confirm' />
-        <div id='alert' />
-      </LanguageHandler>
-    )
-  }
 }
 
-export default nextReduxWrapper.withRedux(WrappedApp)
+// @ts-ignore
+AppComponent.getInitialProps = nextReduxWrapper.getStaticProps(
+  (store) => async (ctx) => {
+    try {
+      await store.dispatch(
+        startupActions.startup({
+          locale: ctx.locale || Constants.defaultLocale,
+        }),
+      )
+    } catch (e) {
+      console.error(e)
+    }
+    return {
+      props: {},
+    }
+  },
+)
+
+export default nextReduxWrapper.withRedux(AppComponent)
