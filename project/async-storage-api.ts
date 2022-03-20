a5a137accf65682662b80ae12a689e311783dde9
kyle-ssg <kyle@solidstategroup.com>
2022-03-20 11:54:27 +0000

    improve web storage

--- project/async-storage-api.ts
+++ project/async-storage-api.ts
@@ -1,8 +1,7 @@
 // import SecuredStorage from 'react-native-secured-storage';
-import cookie from 'cookie'
-import jsCookie from 'js-cookie'
-import { IncomingMessage } from 'http'
 import { API } from './api'
+import nookies from 'nookies'
+import { NextPageContext } from 'next'
 
 const StorageManager = class {
   init = async () => {}
@@ -10,29 +9,29 @@ const StorageManager = class {
     console.error('Web does not support clear cookies')
     return Promise.resolve(false)
   }
-  getItem = function (key: string, req?: IncomingMessage) {
-    API.log('STORAGE', 'GET', key, req)
-    if (req && typeof window === 'undefined') {
-      return Promise.resolve(cookie.parse(req.headers.cookie || '')[key])
-    }
-    return Promise.resolve(jsCookie.get(key) || '')
+  getItem = function (key: string, ctx?: NextPageContext) {
+    API.log('STORAGE', 'GET', key, ctx)
+    return Promise.resolve(nookies.get(ctx)[key])
   }
-  setItem = function (key: string, value: string, req?: IncomingMessage) {
-    if (typeof window === 'undefined' && req) {
-      //todo: set cookie on server, probably need res to do res.setHeader
-      return
-    }
+  setItem = function (key: string, value: string, ctx?: NextPageContext) {
     API.log('STORAGE', 'SET', key, value)
-    return Promise.resolve(jsCookie.set(key, value))
+    return Promise.resolve(nookies.set(ctx, key, value))
   }
-  removeItem = function (key: string, req?: IncomingMessage) {
-    API.log('STORAGE', 'REMOVE', key, req)
-    if (typeof window === 'undefined') {
-      //todo: should be able to set this in nodejs
-      return
-    }
-    jsCookie.remove(key)
-    return Promise.resolve()
+  removeItem = function (key: string, ctx?: NextPageContext) {
+    API.log('STORAGE', 'REMOVE', key, ctx)
+    return Promise.resolve(nookies.destroy(null, key))
+  }
+  getItemSync = function (key: string, ctx?: NextPageContext) {
+    API.log('STORAGE', 'GET', key, ctx)
+    return nookies.get(ctx)[key]
+  }
+  setItemSync = function (key: string, value: string, ctx?: NextPageContext) {
+    API.log('STORAGE', 'SET', key, value)
+    return nookies.set(null, key, value)
+  }
+  removeItemSync = function (key: string, ctx?: NextPageContext) {
+    API.log('STORAGE', 'REMOVE', key, ctx)
+    return nookies.destroy(null, key)
   }
 }
 export default new StorageManager()
