57a194d25b0c9c1c02e47f9d230bf055f2e4d8de
kyle-ssg <kyle@solidstategroup.com>
2022-04-08 08:58:22 +0100

    nprogress

--- components/util/NProgress.tsx
+++ components/util/NProgress.tsx
@@ -1,13 +1,45 @@
-import nProgress from 'nprogress'
 import Router from 'next/router'
-import { FC } from 'react'
 
-Router.events.on('routeChangeStart', () => nProgress.start())
-Router.events.on('routeChangeComplete', () => nProgress.done())
-Router.events.on('routeChangeError', () => nProgress.done())
+if (typeof document !== 'undefined') {
+  const NProgress = require('nprogress')
+  let timer: any
+  let state: string
+  const activeRequests = 0
+  const delay = 100
 
-const NProgress: FC = () => {
-  return <></>
+  // @ts-ignore
+  // eslint-disable-next-line no-inner-declarations
+  function load() {
+    // @ts-ignore
+    if (state === 'loading') {
+      return
+    }
+
+    state = 'loading'
+
+    timer = setTimeout(function () {
+      NProgress.start()
+    }, delay) // only show progress bar if it takes longer than the delay
+  }
+
+  // @ts-ignore
+  // eslint-disable-next-line no-inner-declarations
+  function stop() {
+    if (activeRequests > 0) {
+      return
+    }
+
+    state = 'stop'
+
+    // @ts-ignore
+    clearTimeout(timer)
+    NProgress.done()
+  }
+
+  Router.events.on('routeChangeStart', load)
+  Router.events.on('routeChangeComplete', stop)
+  Router.events.on('routeChangeError', stop)
 }
-
-export default NProgress
+export default function () {
+  return null
+}
