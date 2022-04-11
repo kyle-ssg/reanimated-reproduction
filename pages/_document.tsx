b44c44be8e452e039cb61c4cdc65d17d7a83014a
kyle-ssg <kyle@solidstategroup.com>
2022-04-11 20:30:03 +0100

    remove nunito

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -10,13 +10,7 @@ class MyDocument extends Document {
   render() {
     return (
       <Html>
-        <Head>
-          <link rel='preconnect' href='https://fonts.gstatic.com' />
-          <link
-            href='https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@300;400;700;800&display=swap'
-            rel='stylesheet'
-          />
-        </Head>
+        <Head></Head>
         <body>
           <Main />
           <NextScript />
