73fa7610f306be5bdda3bf95f528aa535c95e549
kyle-ssg <kyle@solidstategroup.com>
2022-08-04 20:52:41 +0100

    align app name

--- mobile/ios/mobile/AppDelegate.mm
+++ mobile/ios/mobile/AppDelegate.mm
@@ -49,7 +49,7 @@ static NSString *const kRNConcurrentRoot = @"concurrentRoot";
 #endif
 
   NSDictionary *initProps = [self prepareInitialProps];
-  UIView *rootView = RCTAppSetupDefaultRootView(bridge, @"boilerplate", initProps);
+  UIView *rootView = RCTAppSetupDefaultRootView(bridge, @"mobile", initProps);
 
   if (@available(iOS 13.0, *)) {
     rootView.backgroundColor = [UIColor systemBackgroundColor];
